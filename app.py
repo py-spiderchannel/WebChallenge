33331ba3c7b23d7e516bc6f9c53c15eebe695564
captain-sina <sinaborhaniit@gmail.com>
2020-11-07 17:05:58 +0330

    func page_not_foun fixed

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def feedback():
 
 @app.errorhandler(404)
 def page_not_found(E):
-  return render_template("page_not_found.html"), 404
+  return render_template("/temp/page_not_found.html"), 404
 
 if __name__ == '__main__':
   app.run()
