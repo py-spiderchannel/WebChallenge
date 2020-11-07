b4e3e9f8e2e96e7903cc334d28068b054d485ea2
captain-sina <sinaborhaniit@gmail.com>
2020-11-07 15:57:38 +0330

    route fixed (home, login, register, Q&A, feedback, about, 404)

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-  return 'hello world'
-  
+  return render_template('/temp/home_form.html')
+
+@app.route('/register')
+def register():
+  return render_template('/temp/register_form.html') 
+
+@app.route('/login')
+def login():
+    return render_template('/temp/login_form.html')
+
+@app.route('/QandA')
+def QandA():
+  return render_template("/temp/QandA_form.html")
+
+@app.route('/about')
+def about():
+  return render_template('/temp/about_form.html')
+
+@app.route('/feedback')
+def feedback():
+  return render_template('/temp/feedback_form.html')
+
+@app.errorhandler(404)
+def page_not_found(E):
+  return render_template("page_not_found.html"), 404
+
 if __name__ == '__main__':
   app.run()
   
